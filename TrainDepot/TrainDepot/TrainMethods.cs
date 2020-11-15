bf953fd65420ddb9de688848b569ab6902cf8f6f
Yaroslav <kachmaryar@gmail.com>
2020-11-16 00:23:42 +0200

    In TrainMethods.cs solving problem of adding station info to the list of stations

--- TrainDepot/TrainDepot/TrainMethods.cs
+++ TrainDepot/TrainDepot/TrainMethods.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+ 
 
 namespace TrainDepot
 {
-    class Class1
+    partial class CTrain
     {
+        public static int trainNumber { get; private set; }
+        public struct SStation
+        {
+            public string stationName { get; set; }
+            public int stationID { get; set; }
+            /*
+            private static implicit operator SStation(string value)
+            {
+                return new SStation() { stationName = value, stationID = index of name in the array };
+            }*/
+        };
+
+        public static List<SStation> stations;
+        //public int currentStationID { get; private set; }
+
+        private void addStations(string[] stationNames, int stationsQuantity) 
+        {
+            //is there a point in repeatedly adding stations one by one?
+            for (int i = 0; i < stationsQuantity; ++i)
+            {
+                SStation sStation = new SStation { stationName = stationNames[i], stationID = Array.IndexOf(stationNames, stationNames[i]) };
+                stations.Add(sStation);
+            }
+        }
+
+
+        // Implement access to initial/final station
+        // or give access directly to field "stations" 
+
+
     }
 }
